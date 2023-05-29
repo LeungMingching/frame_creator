e64f97fc9d8d9c69d9854018aa768488f4d91b84
Liang Mingjing <liangmingjing849@163.com>
2023-05-29 17:14:02 +0800

    initialized agent & agent layer
    Signed-off-by: Liang Mingjing <liangmingjing849@163.com>

---
+++ agent.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class Agent:
+
+    def __init__(self,
+        position: np.ndarray,
+        heading: float,
+        dimention: tuple = (3, 2, 2)
+    ) -> None:
+        pass
---
+++ agent_layer.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+class AgentLayer:
+
+    def __init__(self,
+        distribution_mask: np.ndarray
+    ) -> None:
+        assert len(distribution_mask.shape) == 2
+        
+        self.distribution_mask = distribution_mask
+
+    def update_frenet_grid_range(self,
+        s_min: float,
+        s_max: float,
+        l_min: float,
+        l_max: float
+    ) -> None:
+        s_num = self.distribution_mask.shape[1]
+        l_num = self.distribution_mask.shape[0]
+
+        s_grid = np.linspace(s_min, s_max, s_num)
+        l_grid = np.linspace(l_min, l_max, l_num)
+
+        print(s_grid)
+        print(l_grid)
+
+
+if __name__ == '__main__':
+    distribution_mask = np.zeros((5, 5))
+    al = AgentLayer(
+        distribution_mask=distribution_mask
+    )
+    al.update_frenet_grid_range(0, 100, 0, 12)
