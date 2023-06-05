ba7b92c149809c6893fde994b2061d01cbc2c5ce
Liang Mingjing <liangmingjing849@163.com>
2023-06-05 17:31:52 +0800

    generate squential collection
    Signed-off-by: Liang Mingjing <liangmingjing849@163.com>

--- create_frame_collection.py
+++ create_frame_collection.py
@@ -24,58 +24,87 @@ ego_acceleration = np.zeros((1,2))
 # objects
 
 
-frame_collection = []
 frame = Frame()
 
-# for navi_cmd in tqdm(navi_cmd_grid, desc='Navi Layer: '):
-navi_cmd = np.random.randint(0, 4)
+def generate_squential_collection(
+    navi_cmd_size: int,
+    max_num_ref_line: int,
+    outer_length_range: tuple, # (min, max, num)
+    outer_kappa_range: tuple, # (min, max, num)
+    ego_heading_range: tuple, # (min, max, num)
+    ego_velocity_range: tuple, # (min, max, num)
+    ego_acceleration_range: tuple, # (min, max, num)
+):
+    frame_collection = []
+    
+    navi_cmd_grid = np.linspace(0, navi_cmd_size-1, navi_cmd_size)
 
-for num_line in tqdm(num_ref_line, desc='Reference Line Layer: '):
-    # for outer_l in outer_length:
-    outer_l = 150 + 100*np.random.rand()
-    for outer_r in outer_radius:
+    num_ref_line_grid = np.linspace(1, max_num_ref_line, num=max_num_ref_line, dtype=int)
+    outer_length_grid = np.linspace(outer_length_range[0], outer_length_range[1], num=outer_length_range[2])
+    outer_kappa_grid = np.linspace(outer_kappa_range[0], outer_kappa_range[1], num=outer_kappa_range[2])
+    outer_radius_grid = 1.0 / outer_kappa_grid
+    ego_heading_grid = np.linspace(ego_heading_range[0], ego_heading_range[1], num=ego_heading_range[2])
+    ego_velocity_grid = np.linspace(ego_velocity_range[0], ego_velocity_range[1], num=ego_velocity_range[2])
+    ego_acceleration_grid = np.linspace(ego_acceleration_range[0], ego_acceleration_range[1], num=ego_acceleration_range[2])
+    
+    for navi_cmd in tqdm(navi_cmd_grid, desc='navi_cmd'):
 
-        l_dim = int(2*num_line + 1)
-        s_dim = int(np.floor(outer_l/4.0) + 1)
-        
-        for ego_l_idx in range(l_dim):
-            distribution_mask = np.zeros((l_dim, s_dim))
-            distribution_mask[ego_l_idx][0] = 2
+        for num_ref_line in tqdm(num_ref_line_grid, desc='num_ref_line'):
+            for outer_length in outer_length_grid:
+                for outer_radius in outer_radius_grid:
 
-            # for ego_head in ego_heading:
-            sigma = np.pi * 80 / 180 / 3
-            mu = 0.0
-            ego_head = sigma * np.random.randn() + mu
-            ego_head_array = np.array([ego_head])
+                    l_dim = int(2*num_ref_line + 1)
+                    s_dim = int(np.floor(outer_length/4.0) + 1)
 
-            for ego_v in ego_velocity:
-                ego_v_array = np.array([ego_v])
+                    for ego_l_idx in range(l_dim):
+                        distribution_mask = np.zeros((l_dim, s_dim))
+                        distribution_mask[ego_l_idx][0] = 2
 
-                config = {
-                    'navi_layer': {
-                        'command': navi_cmd,
-                        'execute_distance': np.random.rand() * 300
-                    },
-                    'line_layer': {
-                        'num_line': num_line,
-                        'lane_width': 3.5,
-                        'outer_length': outer_l,
-                        'outer_radius': outer_r,
-                        'step': 0.5
-                    },
-                    'agent_layer': {
-                        'distribution_mask': distribution_mask,
-                        'heading_array': ego_head_array, # (n,)
-                        'velocity_array': ego_v_array, # (n,)
-                        'acceleration_array': ego_acceleration # (n, 2)
-                    }
-                }
+                        for ego_heading in ego_heading_grid:
+                            ego_heading_array = np.array([ego_heading])
 
-                frame.create_frame(config)
-                # frame.draw()
-                frame_dict = frame.get_frame_dict()
-                frame_collection.append(frame_dict)
+                            for ego_velocity in ego_velocity_grid:
+                                ego_velocity_array = np.array([ego_velocity])
 
+                                ego_acceleration_array = np.zeros((1,2))
+
+                                config = {
+                                    'navi_layer': {
+                                        'command': navi_cmd,
+                                        'execute_distance': np.random.rand() * 300
+                                    },
+                                    'line_layer': {
+                                        'num_line': num_ref_line,
+                                        'lane_width': 3.5,
+                                        'outer_length': outer_length,
+                                        'outer_radius': outer_radius,
+                                        'step': 0.5
+                                    },
+                                    'agent_layer': {
+                                        'distribution_mask': distribution_mask,
+                                        'heading_array': ego_heading_array, # (n,)
+                                        'velocity_array': ego_velocity_array, # (n,)
+                                        'acceleration_array': ego_acceleration_array # (n, 2)
+                                    }
+                                }
+
+                                frame.create_frame(config)
+                                # frame.draw()
+                                frame_dict = frame.get_frame_dict()
+                                frame_collection.append(frame_dict)
+
+    return frame_collection
+
+
+frame_collection = generate_squential_collection(
+    navi_cmd_size=4,
+    max_num_ref_line=5,
+    outer_length_range=(150, 250, 5), # (min, max, num)
+    outer_kappa_range=(-0.0025, 0.0025, 8), # (min, max, num)
+    ego_heading_range=(-80/180 * np.pi, -80/180 * np.pi, 5), # (min, max, num)
+    ego_velocity_range=(0, 20, 5), # (min, max, num)
+    ego_acceleration_range=(0, 0, 0), # (min, max, num)
+)
 
 # save
 save_dir = './data'
