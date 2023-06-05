b1645eaafa1b480421b74d13fb2d97bb023f2ffd
Liang Mingjing <liangmingjing849@163.com>
2023-06-05 17:32:06 +0800

    added frame_collection
    Signed-off-by: Liang Mingjing <liangmingjing849@163.com>

---
+++ frame_collection.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+class FrameCollection:
+
+    def __init__(self,
+        navi_cmd_range: np.ndarray,
+        num_ref_line_range: tuple, # (min, max)
+        outer_length_range: tuple,
+
+    ) -> None:
+        pass
