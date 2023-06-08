de7dd348ca543d41fedf504595c557e81269cb53
Liang Mingjing <liangmingjing849@163.com>
2023-06-08 13:28:29 +0800

    added timestamp & adapted BEV
    Signed-off-by: Liang Mingjing <liangmingjing849@163.com>

--- inspect_data.py
+++ inspect_data.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def viz(file_root, file):
+def viz(file_root, file, coordinate_sys='UTM'):
     with open(os.path.join(file_root, file)) as f:
         frame_collection = json.load(f)
 
@@ -14,9 +14,15 @@ def viz(file_root, file):
     fig, ax = plt.subplots(1, 1)
     ax.set_aspect('equal', 'datalim')
 
-    # for batch_idx, data in enumerate(dataloader):
     for frame in tqdm(frame_collection):
         ax.cla()
+        
+        # timestamp
+        timestamp = frame['timestamp']
+        ax.text(.99, .99,
+                f'timestamp: {timestamp}',
+                ha='right', va='top', transform = ax.transAxes)
+
         # navi
         navi_cmd = frame['navi']['command']
         execute_distance = frame['navi']['execute_distance']
@@ -26,15 +32,15 @@ def viz(file_root, file):
 
         # reference lines
         for ref_line in frame['reference_lines']:
-            waypts = ref_line['waypoint']['UTM']
+            waypts = ref_line['waypoint'][coordinate_sys]
             waypts = np.array(waypts)
             x = waypts[:,0]
             y = waypts[:,1]
             ax.plot(x, y, color='g')
         
         # ego
-        ego_position = np.array(frame['ego_status']['position'])
-        ego_heading = np.array(frame['ego_status']['heading'])
+        ego_position = np.array(frame['ego_status']['position']) if coordinate_sys == 'UTM' else (0, 0)
+        ego_heading = np.array(frame['ego_status']['heading']) if coordinate_sys == 'UTM' else 0
         ego_velocity = np.array(frame['ego_status']['velocity'])
 
         ax.text(*ego_position, f'({ego_velocity[0].item():.2f}, {ego_velocity[1].item():.2f})')
@@ -42,9 +48,9 @@ def viz(file_root, file):
 
         # agents
         for ag in frame['agents']:
-            position = np.array(ag['pose']['UTM']['position'])
-            heading = np.array(ag['pose']['UTM']['heading'])
-            velocity = np.array(ag['pose']['UTM']['velocity'])
+            position = np.array(ag['pose'][coordinate_sys]['position'])
+            heading = np.array(ag['pose'][coordinate_sys]['heading'])
+            velocity = np.array(ag['pose'][coordinate_sys]['velocity'])
 
             ax.text(*position, f'({velocity[0].item():.2f}, {velocity[1].item():.2f})')
             ax.quiver(*position, *velocity, color='b')
@@ -59,6 +65,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('file_name', type=str, help='The name of the file to be visualized.')
     parser.add_argument('--file_root', type=str, default='data', help='Diectory that stores the data file.')
+    parser.add_argument('--coordinate_sys', type=str, default='UTM', help='UTM or BEV')
     args = parser.parse_args()
 
-    viz(args.file_root, args.file_name)
+    viz(
+        args.file_root,
+        args.file_name, 
+        args.coordinate_sys
+    )
